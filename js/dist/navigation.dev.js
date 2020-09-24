91b3ac0e9c6623cc0d9d1f0d6d60efdde39b1833
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-09-24 15:16:41 +0200

    feat: add function to open and close drop menu

--- js/dist/navigation.dev.js
+++ js/dist/navigation.dev.js
@@ -1,8 +1,9 @@
-const btnMainMenu = document.getElementById("open-main-menu");
-const mainMenu = document.getElementById("main-menu");
+"use strict";
 
-const btnContextMenu = document.getElementById("open-context-menu");
-const contextMenu = document.getElementById("context-menu");
+var btnMainMenu = document.getElementById("open-main-menu");
+var mainMenu = document.getElementById("main-menu");
+var btnContextMenu = document.getElementById("open-context-menu");
+var contextMenu = document.getElementById("context-menu");
 
 function toggleMainMenu(e) {
   mainMenu.classList.toggle("show");
@@ -12,15 +13,17 @@ function toggleContextMenu(e) {
   contextMenu.classList.toggle("show");
 }
 
-btnMainMenu.addEventListener("click", toggleMainMenu)
-btnContextMenu.addEventListener("click", toggleContextMenu)
+btnMainMenu.addEventListener("click", toggleMainMenu);
+btnContextMenu.addEventListener("click", toggleContextMenu);
 document.addEventListener("click", clickTest);
 
 function clickTest(e) {
   console.log(e.target);
+
   if (e.target !== btnMainMenu) {
     mainMenu.classList.remove("show");
   }
+
   if (e.target !== btnContextMenu) {
     contextMenu.classList.remove("show");
   }
