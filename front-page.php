9cef4fc1d9484491cae2cee025727ab4bf73e562
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-09-25 17:47:02 +0200

    feat: insert front-page markup from static file

--- front-page.php
+++ front-page.php
@@ -1,3 +1,171 @@
 <?php get_header(); ?>
 
+<div class="hero">
+    <div class="container">
+      <span>Moin, ich bin Wiktoria Mielcarek.</span>
+      <h1>Ich erstelle Webseiten die von allen benutzt werden kann.</h1>
+      <span>Web Designer und Entwickler.</span>
+    </div>
+  </div>
+
+  <section class="section-block bg-bb">
+    <div class="container project-section">
+      <h2 class="h1">Meine Projekte</h1>
+        <div class="projects">
+          <div class="project-item" style="background-image: url('../assets/img/Förster_Complimentcard2.jpg')">
+            <a href="#" class="post-link">
+              <h3>Mein Projekt welches ich erarbeitet habe ganz alleine!</h3>
+            </a>
+          </div>
+          <div class="project-item">
+            <a href="#" class="post-link">
+              <h3>Mein Projekt welches ich erarbeitet habe ganz alleine!</h3>
+            </a>
+          </div>
+          <div class="project-item" style="background-image: url('../assets/img/Förster_Complimentcard2.jpg')">
+            <a href="#" class="post-link">
+              <h3>Mein Projekt welches ich erarbeitet habe ganz alleine!</h3>
+            </a>
+          </div>
+        </div>
+        <div class="button-container">
+          <button class="primary" aria-label="Mehr Projekte einsehen">Mehr Projekte <i
+              class="fas fa-long-arrow-alt-right"></i></button>
+        </div>
+    </div>
+  </section>
+
+  <section class="section-block bg-lr">
+    <div class="container about-me">
+      <h2 class="h1">Über Mich</h2>
+      <p>Moin, ich bin Wiktoria! Ich habe Kommunikationsdesign an der HTK in Hamburg studiert. Daraufhin habe ich mich
+        mehr mit Web und UX Design auseinander gesetzt, und mir den Umgang mit Adobe XD, WordPress und Elementor Pro
+        beigebracht. Anschließend habe ich mich noch weitergebildet und HTML und CSS gelernt sowie Online Marketing
+        Tools, wie Funnel Erstellung, Heatmaps, A/B Testing oder Google Ad Kampagnen.</p>
+      <p>Außerdem versuche ich momentan die Ukulele spielen zu lernen.</p>
+    </div>
+  </section>
+
+  <section class="section-block bg-bb">
+    <div class="container blog-section">
+      <h2 class="h1">Blog Beiträge</h2>
+      <div class="blog-posts">
+        <div class="blog-post-item">
+          <a href="#">
+            <h3>Ein Blogbeitrag den ich geschrieben habe.</h3>
+          </a>
+        </div>
+
+        <div class="blog-post-item" style="background-image: url('../assets/img/Förster_Complimentcard2.jpg')">
+          <a href="#">
+            <h3>Ein Blogbeitrag den ich geschrieben habe.</h3>
+          </a>
+        </div>
+      </div>
+      <div class="button-container">
+        <button class="primary">Mehr Blog Beiträge <i class="fas fa-long-arrow-alt-right"></i></button>
+      </div>
+    </div>
+  </section>
+
+  <section class="section-block bg-cr">
+    <div class="container my-skills">
+      <h2 class="h1">Meine Skills</h2>
+      <div class="my-skills-table">
+        <div class="my-skills-cell">
+          <h3>Sprachen</h3>
+          <div class="skills-list">
+            <div class="skill-item">
+              <img src="../assets/img/html5.svg" alt="HTML5 Logo">
+              <span>HTML5</span>
+            </div>
+            <div class="skill-item">
+              <img src="../assets/img/css3.svg" alt="CSS Logo">
+              <span>CSS</span>
+            </div>
+            <div class="skill-item">
+              <img src="../assets/img/javascript.svg" alt="JavaScript Logo">
+              <span>JavaScript</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="my-skills-cell">
+          <h3>Frameworks & Libraries</h3>
+          <div class="skills-list">
+            <div class="skill-item">
+              <img src="../assets/img/react.svg" alt="React Logo">
+              <span>React</span>
+            </div>
+            <div class="skill-item">
+              <img src="../assets/img/WordPress.svg" alt="WordPress Logo">
+              <span>WordPress</span>
+            </div>
+            <div class="skill-item">
+              <img src="../assets/img/bootstrap-4.svg" alt="Bootstrap Logo">
+              <span>Bootstrap</span>
+            </div>
+            <div class="skill-item">
+              <img src="../assets/img/jQuery.svg" alt="jQuery Logo">
+              <span>jQuery</span>
+            </div>
+          </div>
+        </div>
+
+        <div class="my-skills-cell">
+          <h3>Weitere Tools</h3>
+          <div class="skills-list">
+            <div class="skill-item">
+              <img src="../assets/img/Adobe_XD_CC_icon.svg" alt="Adobe XD Logo">
+              <span>Adobe XD</span>
+            </div>
+            <div class="skill-item">
+              <img src="../assets/img/Adobe_Illustrator_CC_icon.svg" alt="Adobe Illustrator Logo">
+              <span>Illustrator</span>
+            </div>
+            <div class="skill-item">
+              <img src="../assets/img/Adobe_Photoshop_CC_icon.svg" alt="Adobe Photoshop Logo">
+              <span>Photoshop</span>
+            </div>
+            <div class="skill-item">
+              <img src="../assets/img/Git.svg" alt="Git Logo">
+              <span>Git</span>
+            </div>
+            <div class="skill-item">
+              <img src="../assets/img/GitHub.svg" alt="GitHub Logo">
+              <span>GitHub</span>
+            </div>
+            <div class="skill-item">
+              <img src="../assets/img/VScode.svg" alt="VS Code Logo">
+              <span>VS Code</span>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </div>
+  </section>
+
+  <section class="section-block bg-lr">
+    <div class="container contact">
+      <h2 class="h1">Kontakt</h2>
+      <form>
+        <label for="name" aria-label="Dein Name"><abbr title="Erforderlich">*</abbr> Dein Name</label>
+        <input name="name" id="name" type="text" placeholder="Wiktoria Mielcarek" required />
+        <label for="email" aria-label="Deine E-Mail Adresse"><abbr title="Erforderlich">*</abbr> Deine E-Mail</label>
+        <input name="email" id="email" type="email" placeholder="w.mielcarek@braweria.de" required />
+        <label for="message" aria-label="Dein Nachricht"><abbr title="Erforderlich">*</abbr> Dein Nachricht</label>
+        <textarea name="message" id="message" rows="8"
+          placeholder="Ich wollte dir nur schreiben, dass Mähnenwölfe toll sind..." required></textarea>
+        <div class="form-privacy-check">
+          <input type="checkbox" name="datenschutz" id="privacy" required>
+          <label for="privacy">Mit der Nutzung dieses Formulars erklärst du dich mit der Speicherung und Verarbeitung
+            deiner Daten durch diese Webseite einverstanden.
+            <a href="#">Klick hier um den Datenschutz zu lesen.</a></label>
+        </div>
+        <button type="submit" class="primary">Nachricht abschicken <i class="fas fa-paper-plane"></i></button>
+      </form>
+    </div>
+  </section>
+
 <?php get_footer(); ?>
