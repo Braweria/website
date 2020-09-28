a674e0a1d2d575d8091d74ded0412311e6434bbb
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-09-28 09:56:17 +0200

    feat: add function for default featured image

--- functions.php
+++ functions.php
@@ -19,4 +19,15 @@ function braweria_wp_init(){
 
 add_action( 'after_setup_theme', 'braweria_wp_init' );
 
+function default_post_metadata__thumbnail_id( $value, $object_id, $meta_key, $single, $meta_type )
+{
+  if ( '_thumbnail_id' == $meta_key )
+  {
+    $value = 45; // the ID for the default image
+  }
+  return $value;
+}
+
+add_filter( 'default_post_metadata', 'default_post_metadata__thumbnail_id', 10, 5 );
+
 ?>
