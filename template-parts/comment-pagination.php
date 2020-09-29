613d5f0675aa854d4c2ddc40b20781d9fa8d2c16
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-09-29 18:40:54 +0200

    feat: create template for comment pagination

---
+++ template-parts/comment-pagination.php
@@ -0,0 +1,13 @@
+<div class="container" role="navigation"> <!--class="button-container-page"-->
+  <button class="primary" aria-label="vorherige Seite"><i class="fas fa-chevron-left"></i>  Vorherige Kommentar Seite
+    <?php previous_comments_link( esc_html__("Vorherige Kommentar Seite", "Braweria WP") ) ?>
+  </button>
+  <!-- <button class="primary" aria-label="Seite wechseln">1</button>
+  <button class="primary" aria-label="Seite wechseln">2</button>
+  <button class="primary active" aria-label="Seite wechseln">3</button>
+  <button class="primary" aria-label="Seite wechseln">4</button>
+  <button class="primary" aria-label="Seite wechseln">5</button> -->
+  <button class="primary" aria-label="Nächste Seite">Nächste Kommentar Seite <i class="fas fa-chevron-right"></i>
+    <?php next_comments_link( esc_html__("Nächste Kommentar Seite", "Braweria WP") ) ?>
+  </button>
+</div>
