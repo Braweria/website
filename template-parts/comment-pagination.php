f49b845aa30473eb4529d940b278990d8d1c0490
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-10-01 11:25:24 +0200

    fix: adjust comment pagi with a tags and not btns

--- template-parts/comment-pagination.php
+++ template-parts/comment-pagination.php
@@ -1,13 +1,9 @@
-<div class="container" role="navigation"> <!--class="button-container-page"-->
-  <button class="primary" aria-label="vorherige Seite"><i class="fas fa-chevron-left"></i>  Vorherige Kommentar Seite
-    <?php previous_comments_link( esc_html__("Vorherige Kommentar Seite", "Braweria WP") ) ?>
-  </button>
+<div class="container comment-pagination" role="navigation"> <!--class="button-container-page"-->
+<?php previous_comments_link( esc_html__("Vorherige Kommentar Seite", "Braweria WP") ) ?>
   <!-- <button class="primary" aria-label="Seite wechseln">1</button>
   <button class="primary" aria-label="Seite wechseln">2</button>
   <button class="primary active" aria-label="Seite wechseln">3</button>
   <button class="primary" aria-label="Seite wechseln">4</button>
   <button class="primary" aria-label="Seite wechseln">5</button> -->
-  <button class="primary" aria-label="Nächste Seite">Nächste Kommentar Seite <i class="fas fa-chevron-right"></i>
-    <?php next_comments_link( esc_html__("Nächste Kommentar Seite", "Braweria WP") ) ?>
-  </button>
+  <?php next_comments_link( esc_html__("Nächste Kommentar Seite", "Braweria WP") ) ?>
 </div>
