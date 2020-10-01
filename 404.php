19e9b2f29ab7a6d0a133ca6833f3b1286902df1c
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-10-01 11:03:41 +0200

    feat: add and style the 404 page

--- 404.php
+++ 404.php
@@ -1,3 +1,10 @@
 <?php get_header(); ?>
 
+<div class="four-oh-four bg-lr">
+
+<h1>Oh-Oh!</h1>
+<p>Sieht aus, als hätte der Mähnenwolf wohl eine Verbindung zerkaut...</p>
+
+</div>
+
 <?php get_footer(); ?>
