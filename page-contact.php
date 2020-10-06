b9940bec96fd817ddfd447c8b54ff286c8249ef6
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-10-06 13:11:39 +0200

    feat: add ARIA label to links

--- page-contact.php
+++ page-contact.php
@@ -7,10 +7,10 @@
       Willst du mir lieber nur Hallo sagen und ein bisschen chatten? Dann schicke mir eine Private Nachricht auf meinen Social Media Kanälen.</p>
       <?php echo do_shortcode('[contact-form-7 id="52" title="kontakt-frontpage"]'); ?>
       <div class="contact-social">
-        <a href="https://twitter.com/Braweria"><button class="primary"><i class="fab fa-twitter"></i></button></a>
-        <a href="https://www.behance.net/braweria"><button class="primary"><i class="fab fa-behance"></i></button></a>
-        <a href="https://github.com/Braweria"><button class="primary"><i class="fab fa-github"></i></button></a>
-        <a href="https://www.reddit.com/u/braweria"><button class="primary"><i class="fab fa-reddit-alien"></i></button></a>
+        <a href="https://twitter.com/Braweria" aria-label="Zu meinem Twitter wechseln"><button class="primary"><i class="fab fa-twitter"></i></button></a>
+        <a href="https://www.behance.net/braweria" aria-label="Zu meinem Behance wechseln"><button class="primary"><i class="fab fa-behance"></i></button></a>
+        <a href="https://github.com/Braweria" aria-label="Zu meinem Github wechseln"><button class="primary"><i class="fab fa-github"></i></button></a>
+        <a href="https://www.reddit.com/u/braweria" aria-label="Zu meinem Reddit wechseln"><button class="primary"><i class="fab fa-reddit-alien"></i></button></a>
       </div>
     </div>
   </section>
