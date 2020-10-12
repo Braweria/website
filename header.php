4f7105b216ce519c6ae53bfeb0fadd5ffcc35af5
Wiktoria Mielcarek <62669899+Braweria@users.noreply.github.com>
2020-10-12 09:44:44 +0200

    fix: remove fixed title tag from header

--- header.php
+++ header.php
@@ -4,12 +4,8 @@
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Braweria WP Theme</title>
 
-  <!-- <link href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,600;0,700;0,800;1,300;1,400;1,600;1,700;1,800&display=swap" rel="stylesheet">
-  <script src="https://kit.fontawesome.com/5659311191.js" crossorigin="anonymous"></script>
-  <link href="style.css" rel="stylesheet" type="text/css">
-  <script src="assets/js/navigation.js" defer></script> -->
+
   <?php wp_head(); ?>
   <?php $upload_dir = wp_upload_dir(); ?>
 </head>
